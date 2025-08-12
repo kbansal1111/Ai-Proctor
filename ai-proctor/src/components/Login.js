9de471ced677e45eb7cae4836525f55c6a9ea7b0
Kartik Bansal <kartikbansal9152@gmail.com>
2025-08-13 00:12:23 +0530

    fix bugs

--- ai-proctor/src/components/Login.js
+++ ai-proctor/src/components/Login.js
@@ -13,7 +13,6 @@ export default function Login() {
         e.preventDefault();
         setError("");
         setIsLoading(true);
-//"http://127.0.0.1:5000/login"
         const loginData = { username, rollNumber, password };
         fetch("http://13.53.233.58:5000/login", {
             method: "POST",
